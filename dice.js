ed26c1ae821d505407857b08caa2210f7698b468
goutham1202 <66524881+goutham1202@users.noreply.github.com>
2024-07-16 13:28:21 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -9,9 +9,9 @@ function rollDice() {
 
     // Update the header text based on the dice rolls
     if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+        document.querySelector("h1").textContent = "Player 1 Wins!";
     } else if (randomNumber2 > randomNumber1) {
-        document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+        document.querySelector("h1").textContent = "Player 2 Wins!";
     } else {
         document.querySelector("h1").textContent = "Draw";
     }
